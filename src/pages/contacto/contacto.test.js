f941f8e8f1d3cf5bf28d1b18806e9ee0fefcd83a
Miguel <miguel@santacc.es>
2017-12-20 16:12:53 +0100

    Skip snapshot tests in Travis.

--- src/pages/contacto/contacto.test.js
+++ src/pages/contacto/contacto.test.js
@@ -6,7 +6,7 @@ import ContactoPage from './index'
 import Adapter from 'enzyme-adapter-react-15'
 Enzyme.configure({ adapter: new Adapter() })
 
-const testIf = process.env.MODE && process.env.MODE === 'skipsnapshots' ? test.skip : test
+const testIf = process.env.MODE !== undefined && process.env.MODE === 'skipsnapshots' ? test.skip : test
 
 testIf('Mostrar el HTML del componente', () => {
   const wrapper = shallow(
